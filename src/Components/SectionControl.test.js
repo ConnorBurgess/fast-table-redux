7ec234e68b6da272fe672c00afebbbb2829a9903
Connor Burgess <connorburgesscodes@gmail.com>
2021-04-23 13:25:48 -0700

    pass render test for SectionControl

--- src/Components/SectionControl.test.js
+++ src/Components/SectionControl.test.js
@@ -2,9 +2,9 @@ import React from "react";
 import { shallow, mount, configure } from "enzyme";
 import toJson from "enzyme-to-json";
 import Adapter from 'enzyme-adapter-react-16';
-import NewSectionForm from "./NewSectionForm";
+import SectionControl from "./SectionControl";
 configure({adapter: new Adapter()});
 
-it('renders NewSectionForm without crashing', () => {
-  shallow(<NewSectionForm />);
+it('renders SectionControl without crashing', () => {
+  shallow(<SectionControl />);
 });
