8202bd8871f10b2dda6952b15dd2b0c500cb9cd8
Connor Burgess <connorburgesscodes@gmail.com>
2021-05-09 12:03:53 -0700

    add action creators

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import * as c from './ActionTypes'
+export const deleteSection = id => ({
+  type: c.DELETE_SECTION,
+  id
+});
+
+export const updateSection = id => ({
+  type: c.DELETE_SECTION,
+  id
+});
+
+export const addSection = section => {
+  const {name, tableCount, originalCount, id} = section;
+
+  return{
+  type: c.ADD_SECTION,
+  name: name,
+  tableCount: tableCount,
+  originalCount: originalCount,
+  id: id
+}};
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM
+});
+
