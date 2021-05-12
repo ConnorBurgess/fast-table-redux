0f17e771692bd1b5589b99f8dc9026c361f2c321
Connor Burgess <connorburgesscodes@gmail.com>
2021-05-11 23:23:46 -0700

    pass test for updateForm action

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,10 +7,16 @@ describe('FastTable actions', () => {
       id: 1
     });
   });
-  
+
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
       type: 'TOGGLE_FORM'
+    });
+  });
+  
+  it('updateForm should create UPDATE_FORM action', () => {
+    expect(actions.updateForm()).toEqual({
+      type: 'UPDATE_FORM'
     });
   });
 
